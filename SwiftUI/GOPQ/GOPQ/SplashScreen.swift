7f357eef48fa8f85b9e91c60f10805f3c064538c
Dikoi <dicky.susanto@binus.ac.id>
2025-03-25 11:56:53 +0700

    feat: add splash screen page

---
+++ SwiftUI/GOPQ/GOPQ/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  GOPQ
+//
+//  Created by Dicky Dharma Susanto on 25/03/25.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text("Hello Worlds")
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
