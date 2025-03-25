1e3c24d4c8489a0ab5abc6528d6320b752846aa5
Shawn Andrew <shawnandrew@10-60-38-223.0100.bsd.id.iosda.org>
2025-03-25 07:46:58 +0700

    Added Base Folders

---
+++ SwiftUI/GOPQ/GOPQ/GOPQApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOPQApp.swift
+//  GOPQ
+//
+//  Created by Shawn Andrew on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GOPQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/GOPQ/GOPQTests/GOPQTests.swift
@@ -0,0 +1,17 @@
+//
+//  GOPQTests.swift
+//  GOPQTests
+//
+//  Created by Shawn Andrew on 25/03/25.
+//
+
+import Testing
+@testable import GOPQ
+
+struct GOPQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
