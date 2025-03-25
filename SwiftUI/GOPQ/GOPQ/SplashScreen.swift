0e2f0e7c8c5b98683c2d9f495d5195b47cb751ef
yehezkieljoseph <joseph.widianto@gmail.com>
2025-03-25 12:06:15 +0700

    feat: splash screen finished

--- SwiftUI/GOPQ/GOPQ/SplashScreen.swift
+++ SwiftUI/GOPQ/GOPQ/SplashScreen.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct SplashScreen: View {
+    @State private var nama:String = ""
     var body: some View {
-        Text("Hello Worlds")
+        ZStack{
+            Color.black.ignoresSafeArea()
+            
+            VStack {
+                Image("gopq")
+                    .resizable()
+                    .frame(width:90,height:150)
+                    
+                
+                Text("Silahkan Masukkan Nama Lengkap Anda!")
+                    .foregroundStyle(.white)
+                
+                TextField("Username", text: $nama)
+                    .padding(10)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .frame(width:200)
+                    .cornerRadius(15)
+                
+                Button(action: {
+                    print("diklik")
+                })
+                {
+                    Text("Konfirmasi")
+                        .padding(10)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .frame(width:300)
+                        .padding(.top)
+                       
+                }
+
+                
+            }
+        }
     }
 }
 
